e5df48e5174b99a2eba991e512bc79997f658023
Pitchfork :3 <kanonenwagen7@gmail.com>
2026-02-06 14:11:01 -0500

    added log main.py

--- main.py
+++ main.py
@@ -145,6 +145,13 @@ def solve():
         if random.randint(1,chanceofmess) == 1:
             a =  messup(a)
         awnser.config(text = a)
+    elif x[0] == "log" or x[0] == "logarithm":
+        base = int(x[1])
+        target = int(x[1])
+        a = log(target, base)
+        if random.randint(1,chanceofmess) == 1:
+            a =  messup(a)
+        awnser.config(text = a)
     else:
         if len(x) == 3:
             if x[0] == "pi":
