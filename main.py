71638650f64f1b0c5d7879990799105bf9d3c3fe
Pitchfork :3 <kanonenwagen7@gmail.com>
2026-02-06 08:44:34 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ dumbness = 5
 jeffsayings = {"mrrrrrp", "grrrrrr", "murp", "gurp", "RAWR", "MRRRRRRRRRf"}
 
 
+def error():
+    pass
+
 def leave():
     root.destroy()
 
@@ -46,30 +49,35 @@ def solve():
     s = x[1]
     b = int(x[2])
 
-    if s == "+":
-        d = add(a, b)
-        t = True
-    elif s == "-":
-        d = minus(a, b)
-        t = True
-    elif s == "*":
-        d = times(a, b)
-        t = True
-    elif s == "/" or s == "%":
-        d = divide(a, b)
-        t = True
+    if len(x) == 3:
+
+        if s == "+":
+            d = add(a, b)
+            t = True
+        elif s == "-":
+            d = minus(a, b)
+            t = True
+        elif s == "*":
+            d = times(a, b)
+            t = True
+        elif s == "/" or s == "%":
+            d = divide(a, b)
+            t = True
+        else:
+            t = False
+            d = list(jeffsayings)[0]
+            awnser.config(text = d)
+    
+        if t == True:
+            isdumb = random.randint(1,2)
+            if isdumb == 1:
+                d = messup(d)
+                awnser.config(text = d)
+            else:
+                awnser.config(text = d)
     else:
-        t = False
         d = list(jeffsayings)[0]
         awnser.config(text = d)
-
-    if t == True:
-        isdumb = random.randint(1,2)
-        if isdumb == 1:
-            d = messup(d)
-            awnser.config(text = d)
-        else:
-            awnser.config(text = d)
 
 
     
@@ -83,7 +91,7 @@ root.geometry("200x300")
 #widgits being created
 exitb = tk.Button(root, command = leave, text = "exit")
 entrybox = tk.Entry(root, width = 15,)
-solvebutton = tk.Button(root, command = leave, text = "solve")
+solvebutton = tk.Button(root, command = solve, text = "solve")
 awnser = tk.Label(root, text = list(jeffsayings)[0])
 
 #add widget to windows :3
